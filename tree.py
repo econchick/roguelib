68d0a2ddf324d2b5dc9ad2ef922f891f54391d90
Lynn Root <erin.lynn.root@gmail.com>
2012-04-10 20:57:59 -0700

    Refactored Tree File

--- tree.py
+++ tree.py
@@ -6,6 +6,9 @@ class TreeNode:
 		self.parent = None
 		self.children = []
 
+	def __repr__(self):
+		return str(self.value)
+
 def link(parent, child):
 	parent.children.append(child)
 	child.parent = parent
@@ -19,15 +22,54 @@ class BinaryTree:
 
 		self.root = root
 
+
+class TreeAdapter:
+	def __init__(self):
+		self.deque = deque()
+
+	def add(self, value):
+		self.deque.appendleft(value)
+
+	def __len__(self):
+		return len(self.deque)
+
+class StackAdapter(TreeAdapter):
+	def remove(self):
+		return self.deque.popleft()
+
+class QueueAdapter(TreeAdapter):
+	def remove(self):
+		return self.deque.pop()
+
+class TreeIterator:
+	def __init__(self, structure_adapter, root):
+		self.structure_adapter = structure_adapter
+		self.root = root
+
+	def __iter__(self):
+		to_process = self.structure_adapter
+		to_process.add(self.root)
+		while len(to_process) != 0:
+			current = to_process.remove()
+			yield current
+			for child in current.children:
+				to_process.add(child)
+
+class DepthFirstIterator(TreeIterator):
+	def __init__(self, root):
+		TreeIterator.__init__(self, StackAdapter(), root)
+
+class BreadthFirstIterator(TreeIterator):
+	def __init__(self, root):
+		TreeIterator.__init__(self, QueueAdapter(), root)
+
+def TreeSearch(root, value, iterator):
+	for node in iterator(root):
+		if value == node.value:
+			return node
+
+def BreadthFirstSearch(root, value):
+	return TreeSearch(root, value, BreadthFirstIterator)
+
 def DepthFirstSearch(root, value):
-	stack = deque()
-	stack.appendleft(root)
-	while len(stack) != 0:
-		current = stack.pop()
-		if current.value == value:
-			return current
-
-		for child in current.children:
-			stack.appendleft(child)
-
-	return None
+	return TreeSearch(root, value, DepthFirstIterator)
